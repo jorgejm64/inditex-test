f017ba0eb09a227532c107b69685399ced08a687
Jorge Jimenez Mejias <82114002+jorgejm64@users.noreply.github.com>
2023-03-26 13:37:35 +0100

    Fetching Podcast in the Homepage
    Added the API fetching in HomePage to retrieve all the podcasts data.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,33 @@
+//React
+import { useEffect, useState } from "react";
+
 //Styles
 import styles from "./HomePage.module.scss";
 
 const HomePage = () => {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<any>();
+    const [error, setError] = useState();
+
+    const apiUrlAllPodcast =
+        "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json";
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(apiUrlAllPodcast)
+            .then((response) => response.json())
+            .then((data) => setData(data))
+            .catch((e) => {
+                console.log(e);
+                setError(e);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    if (!loading) {
+        console.log(data);
+    }
+
     return (
         <section className={styles.homePage}>
             <h1>HomePage</h1>
