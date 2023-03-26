33982da5a5cf218bb437350f9f4fc944469305bc
Jorge Jimenez Mejias <82114002+jorgejm64@users.noreply.github.com>
2023-03-26 13:22:11 +0100

    Setting the main.tsx
    Adding the router to the Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+//React
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-  </React.StrictMode>,
-)
+//Styles
+import "./styles/globals.scss";
+
+//Router
+import { router } from "./router/router";
+import { RouterProvider } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
